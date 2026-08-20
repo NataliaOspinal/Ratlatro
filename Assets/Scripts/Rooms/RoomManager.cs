93de52ff21295a0ccb365da4e60c7fbee92304cf
Liz Pineda <a20240313@pucp.edu.pe>
2026-08-19 22:32:01 -0500

    room prefab

--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -1,16 +1,47 @@
-using UnityEngine;
+using UnityEngine; 
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 
 public class RoomManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public static RoomManager Instance { get; private set; }
+
+    [Header("Room Data")]
+    public List<GameObject> roomPrefabs; 
+    private GameObject currentRoom;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance=this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        currentRoom=GameObject.Find("ROOM");
+    }
+
+    public void LoadNextRoom(Door.PuertaDireccion exitDirection, GameObject player)
+    {
+        if (currentRoom != null)
+        {
+            Destroy(currentRoom);
+        }
+
+        // Elegir una sala aleatoria de la lista
+        int randomI=Random.Range(0,roomPrefabs.Count);
+        GameObject roomToload = roomPrefabs[randomI];
+
+        currentRoom=Instantiate(roomToload, Vector3.zero, UnityEngine.Quaternion.identity);
+
+        UnityEngine.Debug.Log("Estamos saliendo por la puerta: " + exitDirection);
     }
 }

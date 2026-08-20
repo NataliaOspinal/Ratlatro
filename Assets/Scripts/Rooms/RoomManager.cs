9a4509a2d9b41e6f070242791eab109fb4d13294
Liz Pineda <a20240313@pucp.edu.pe>
2026-08-20 01:25:11 -0500

    Barrido
    Falta arreglar

--- Assets/Scripts/Rooms/RoomManager.cs
+++ Assets/Scripts/Rooms/RoomManager.cs
@@ -1,66 +1,74 @@
-using UnityEngine; 
+using UnityEngine;
+using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
 
 public class RoomManager : MonoBehaviour
 {
-
     public static RoomManager Instance { get; private set; }
 
     [Header("Room Data")]
-    public List<GameObject> roomPrefabs; 
+    public List<GameObject> roomPrefabs;
     private GameObject currentRoom;
+
+    [Header("Transicion de Barrido")]
+    public Animator panelAnimator;
+    public float tiempoDeEspera = 0.5f; 
+
+    private bool isTransitioning = false;
 
     private void Awake()
     {
-        if (Instance != null && Instance != this)
-        {
-            Destroy(this);
-        }
-        else
-        {
-            Instance=this;
-        }
+        if (Instance != null && Instance != this) Destroy(this);
+        else Instance = this;
     }
 
     private void Start()
     {
-        currentRoom=GameObject.Find("ROOM");
+        if (currentRoom == null) currentRoom = GameObject.Find("ROOM");
     }
 
     public void LoadNextRoom(Door.PuertaDireccion exitDirection, GameObject player)
     {
+        if (isTransitioning) return; 
+        
+        StartCoroutine(RutinaCambioSala(exitDirection, player));
+    }
+
+    private IEnumerator RutinaCambioSala(Door.PuertaDireccion exitDirection, GameObject player)
+    {
+        isTransitioning = true;
+
+        if (panelAnimator != null)
+        {
+            panelAnimator.SetTrigger("CambiarSala");
+        }
+
+        yield return new WaitForSeconds(tiempoDeEspera);
+
+        Vector3 posicionSala = Vector3.zero;
         if (currentRoom != null)
         {
+            posicionSala = currentRoom.transform.position;
+            currentRoom.SetActive(false); 
             Destroy(currentRoom);
         }
 
-        // Elegir una sala aleatoria de la lista
-        int randomI=Random.Range(0,roomPrefabs.Count);
-        GameObject roomToload = roomPrefabs[randomI];
-
-        currentRoom=Instantiate(roomToload, Vector3.zero, UnityEngine.Quaternion.identity);
+        int randomIndex = Random.Range(0, roomPrefabs.Count);
+        GameObject roomToLoad = roomPrefabs[randomIndex];
+        currentRoom = Instantiate(roomToLoad, posicionSala, Quaternion.identity);
 
         Transform spawnFolder = currentRoom.transform.Find("SpawnPts");
-        Transform targetSpawn = null;
-
-        if (exitDirection == Door.PuertaDireccion.Right)
+        if (spawnFolder != null)
         {
-            targetSpawn = spawnFolder.Find("Spawn_L"); 
-        }
-        else if (exitDirection == Door.PuertaDireccion.Left)
-        {
-            targetSpawn = spawnFolder.Find("Spawn_R"); 
+            Transform targetSpawn = (exitDirection == Door.PuertaDireccion.Right) ? spawnFolder.Find("Spawn_L") : spawnFolder.Find("Spawn_R");
 
+            if (targetSpawn != null)
+            {
+                player.transform.position = targetSpawn.position;
+            }
         }
 
-        if (targetSpawn != null)
-        {
-            player.transform.position = targetSpawn.position;
-        }
-        else
-        {
-            Debug.LogWarning("Faltan Spawns");
-        }
+        yield return new WaitForSeconds(0.1f);
+        isTransitioning = false;
     }
 }

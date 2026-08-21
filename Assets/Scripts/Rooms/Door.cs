d9d316b470f60b1d7aba79d25c9d893518e43c47
Liz Pineda <a20240313@pucp.edu.pe>
2026-08-20 23:50:45 -0500

    sonidos, puertas, animaciones, dialogo

--- Assets/Scripts/Rooms/Door.cs
+++ Assets/Scripts/Rooms/Door.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Door : MonoBehaviour
@@ -5,11 +6,45 @@ public class Door : MonoBehaviour
     public enum PuertaDireccion {Right, Left}
     public PuertaDireccion direccion;
 
+    [Header("Animación y Efectos")]
+    public Animator puertaAnimator; 
+    public string nombreTriggerAnimacion = "Abrir"; 
+    
+    public float duracionTemblor = 0.3f; 
+    public float fuerzaTemblor = 0.03f;  
+
+    private bool puertaActivada = false;
+
+
+    public void Abrir()
+    {
+        if (puertaAnimator != null)
+        {
+            puertaAnimator.SetTrigger(nombreTriggerAnimacion);
+        }
+        
+        StartCoroutine(EfectoTemblor());
+    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && !puertaActivada)
         {
+            puertaActivada = true; 
             RoomManager.Instance.LoadNextRoom(direccion, collision.gameObject);
         }
+    }
+
+    private IEnumerator EfectoTemblor()
+    {
+        Vector3 posicionOriginal = transform.position;
+        float tiempo = 0f;
+        while (tiempo < duracionTemblor)
+        {
+            transform.position = posicionOriginal + (Vector3)Random.insideUnitCircle * fuerzaTemblor;
+            tiempo += Time.deltaTime;
+            yield return null;
+        }
+        transform.position = posicionOriginal;
     }
 }

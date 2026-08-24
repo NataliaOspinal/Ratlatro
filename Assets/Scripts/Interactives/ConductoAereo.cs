91f0185e8b2fdd6e744943f33c7951fb3ad0570f
NataliaOspinal <226923708+NataliaOspinal@users.noreply.github.com>
2026-08-23 22:23:09 -0500

    Fixing + room 8
    Correcciones

--- Assets/Scripts/Interactives/ConductoAereo.cs
+++ Assets/Scripts/Interactives/ConductoAereo.cs
@@ -1,8 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
 
 public class ConductoAereo : MonoBehaviour
 {
     public Transform puntoDeAterrizaje;
+
+    //Nombre de escena destino (si existe) para teletransportar al jugador a otra escena
+    public string escenaDestino;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -12,6 +17,20 @@ public class ConductoAereo : MonoBehaviour
         {
             // Detiene el vuelo, cancela la gravedad y la teletransporta suavemente
             rata.AterrizajePerfecto(puntoDeAterrizaje.position);
+
+            // Si le indicamos un nombre de escena en el Inspector, iniciamos el viaje
+            if (!string.IsNullOrEmpty(escenaDestino))
+            {
+                StartCoroutine(ViajarAEscena());
+            }
         }
+    }
+
+    private IEnumerator ViajarAEscena()
+    {
+        // Un pequeñísimo retraso (0.5 segundos) para que el jugador alcance a ver a la rata entrando al ducto
+        yield return new WaitForSeconds(0.5f);
+
+        SceneManager.LoadScene(escenaDestino);
     }
 }

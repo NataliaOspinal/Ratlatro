6c213c14d8c58ca4b1bab403ecd0bfdaa5339188
Liz Pineda <a20240313@pucp.edu.pe>
2026-08-30 14:37:25 -0500

    camara zoomOut

--- Assets/Scripts/Rooms/CameraSway.cs
+++ Assets/Scripts/Rooms/CameraSway.cs
@@ -11,8 +11,8 @@ public class CameraSway : MonoBehaviour
     public float suavidad=3f;
 
     [Header("Configuracion de Zoom")]
-    public float zoomNormal=10f;
-    public float zoomAcercado=6f;
+    public float zoomNormal=6f;
+    public float zoomAlejado=10f;
     public float suavidadZoom=5f;
 
     [Range(0f,1f)]
@@ -29,39 +29,35 @@ public class CameraSway : MonoBehaviour
         if (cam != null) zoomNormal = cam.orthographicSize;
     }
 
-    void Update()
+    void LateUpdate()
     {
         if (Mouse.current == null || Keyboard.current == null || cam == null) return;
-        bool estaAcercado = Keyboard.current.leftShiftKey.isPressed;
-
-        float zoomDestino = estaAcercado ? zoomAcercado : zoomNormal;
+        
+        bool estaAlejando = Keyboard.current.leftShiftKey.isPressed;
+        float zoomDestino = estaAlejando ? zoomAlejado : zoomNormal;
         cam.orthographicSize = Mathf.Lerp(cam.orthographicSize, zoomDestino, suavidadZoom * Time.deltaTime);
 
         Vector3 centroDestino = posicionInicial;
 
-        if (estaAcercado)
+        if (jugador != null)
         {
-            if (jugador != null)
-            {
-               centroDestino = new Vector3(jugador.position.x, jugador.position.y, posicionInicial.z);
-            }
-            else
-            {
-                Debug.LogWarning("Falta rata");
-            }
+            centroDestino = new Vector3(jugador.position.x, jugador.position.y, posicionInicial.z);
+        }
+        else
+        {
+            Debug.LogWarning("Falta rata");
         }
 
-
         Vector2 mousePos = Mouse.current.position.ReadValue();
-        float mouseX=(mousePos.x/Screen.width)-0.5f;
-        float mouseY=(mousePos.y/Screen.height)-0.5f;
+        float mouseX = (mousePos.x / Screen.width) - 0.5f;
+        float mouseY = (mousePos.y / Screen.height) - 0.5f;
 
-      Vector3 posicionDestino = new Vector3(
+        Vector3 posicionDestino = new Vector3(
             centroDestino.x + (mouseX * cantidadMovimiento),
             centroDestino.y + (mouseY * cantidadMovimiento),
             posicionInicial.z
         );
     
-        transform.position = Vector3.Lerp(transform.position, posicionDestino, suavidad * Time.deltaTime);    }
-
+        transform.position = Vector3.Lerp(transform.position, posicionDestino, suavidad * Time.deltaTime);    
+    }
 }
